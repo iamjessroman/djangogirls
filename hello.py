9c114389d606f9aa9d2ef44f0906a797998aa330
jessicaroman <jessicaromanlargo@gmail.com>
2017-09-02 16:19:45 -0500

    first commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+fruits=['orange','apple','watermelon']
+
+def fruitsNames(name):
+	print("Hi!"+ name)
+	for fruit in fruits:
+		if fruits[1]=='apple':
+			print('I love apple \n')
+		print('Hello! I am a ' + fruit)
+
+fruitsNames('Jessica')
